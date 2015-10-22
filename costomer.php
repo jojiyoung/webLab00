34fa98b91873721bca7eb1c0713dbeff10600b29
jojiyoung <dhtks4341@naver.com>
2015-10-22 14:16:57 +0900

    costomer.php

---
+++ costomer.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fruit Store</title>
+		<link href="http://selab.hanyang.ac.kr/courses/cse326/2015/problems/pResources/gradestore.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<?php
+		# Ex 4 : 
+	
+		# Check the existance of each parameter using the PHP function 'isset'.
+		# Check the blankness of an element in $_POST by comparing it to the empty string.
+		# (can also use the element itself as a Boolean test!)
+		# if (){
+		?>
+
+		<!-- Ex 4 : 
+			Display the below error message : 
+			<h1>Sorry</h1>
+			<p>You didn't fill out the form completely. Try again?</p>
+		--> 
+
+		<?php
+		# Ex 5 : 
+		# Check if the name is composed of alphabets, dash(-), ora single white space.
+		# } elseif () { 
+		?>
+
+		<!-- Ex 5 : 
+			Display the below error message : 
+			<h1>Sorry</h1>
+			<p>You didn't provide a valid name. Try again?</p>
+		--> 
+
+		<?php
+		# Ex 5 : 
+		# Check if the credit card number is composed of exactly 16 digits.
+		# Check if the Visa card starts with 4 and MasterCard starts with 5. 
+		# } elseif () {
+		?>
+
+		<!-- Ex 5 : 
+			Display the below error message : 
+			<h1>Sorry</h1>
+			<p>You didn't provide a valid credit card number. Try again?</p>
+		--> 
+
+		<?php
+		# if all the validation and check are passed 
+		# } else {
+		?>
+
+		<h1>Thanks!</h1>
+		<p>Your information has been recorded.</p>
+		
+		<!-- Ex 2: display submitted data -->
+		<ul> 
+			<li>Name: <?echo $data['membership_name'] = $_POST['membership_name'];?></li>
+			<li>Membership Number: <?echo $data['membership_number'] = $_POST['membership_number'];?> </li>
+			<li>Fruits:  <?echo $data['check'] = $_POST['check'];?></li>
+			<li>Credit:<?echo $data['creat_number'] = $_POST['creat_number'];?> </li>
+		</ul>
+		
+		<!-- Ex 3 : 
+			<p>This is the sold fruits count list:</p> -->
+		<?php
+			$filename = "customers.txt";
+			/* Ex 3: 
+			 * Save the submitted data to the file 'customers.txt' in the format of : "name;membershipnumber;fruit;number".
+			 * For example, "Scott Lee;20110115238;apple;3"
+			 */
+		?>
+		
+		<!-- Ex 3: list the number of fruit sold in a file "customers.txt".
+			Create unordered list to show the number of fruit sold -->
+		<ul>
+		<?php 
+		#$fruitcounts = soldFruitCount($filename);
+		#foreach() {
+		?>
+		<!-- <li></li> -->
+		<?php
+		#}
+		?>
+		</ul>
+		
+		<?php
+		# }
+		?>
+		
+		<?php
+			/* Ex 3 :
+			* Get the fruits species and the number from "customers.txt"
+			* 
+			* The function parses the content in the file, find the species of fruits and count them.
+			* The return value should be an key-value array
+			* For example, array("Melon"=>2, "Apple"=>10, "Orange" => 21, "Strawberry" => 8)
+			*/
+			function soldFruitCount($filename) { }
+		?>
+		
+	</body>
+</html>
